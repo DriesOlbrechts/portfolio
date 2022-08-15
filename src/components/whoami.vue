c2eb814976c2772f043efa385f82b0f895a7ca80
Dries1234 <driesolbrechts@gmail.com>
2022-08-15 23:39:56 +0200

    animation

--- src/components/whoami.vue
+++ src/components/whoami.vue
@@ -29,10 +29,10 @@ import VueFeather from 'vue-feather';
     display: flex;
     -webkit-box-orient: horizontal;
     -webkit-box-direction: normal;
-        -ms-flex-direction: row;
-            flex-direction: row;
+    -ms-flex-direction: row;
+    flex-direction: row;
     -ms-flex-pack: distribute;
-        justify-content: space-around;
+    justify-content: space-around;
     margin: auto;
     margin-bottom: 1rem;
     width: 20rem;
@@ -45,9 +45,9 @@ import VueFeather from 'vue-feather';
     border-radius: 0.5rem;
 }
 
-.icons > a:hover{
-    -webkit-filter: drop-shadow(0 0 0.7rem rgb(255,255,255));
-            filter: drop-shadow(0 0 0.7rem rgb(255,255,255));
+.icons>a:hover {
+    -webkit-filter: drop-shadow(0 0 0.7rem rgb(255, 255, 255));
+    filter: drop-shadow(0 0 0.7rem rgb(255, 255, 255));
 }
 
 .greeting {
@@ -61,37 +61,43 @@ import VueFeather from 'vue-feather';
 .subtitle {
     width: 20rem;
     -webkit-filter: brightness(75%);
-            filter: brightness(75%);
+    filter: brightness(75%);
     font-size: 2rem;
     overflow: hidden;
     white-space: nowrap;
     border-right: 0.15rem solid var(--primary-accent);
     padding-right: 0.5rem;
-    -webkit-animation: typing 2s steps(40, end),blink 1s step-end infinite;
-            animation: typing 2s steps(40, end),blink 1s step-end infinite;
+    -webkit-animation: typing 2s steps(40, end), blink 1s step-end infinite;
+    animation: typing 2s steps(40, end), blink 1s step-end infinite;
 }
 
 
 
 @-webkit-keyframes typing {
-    from {width: 0}
+    from {
+        width: 0
+    }
 }
 
 
 
 @keyframes typing {
-    from {width: 0}
+    from {
+        width: 0
+    }
 }
+
 @-webkit-keyframes blink {
 
     50% {
-       border-color: transparent; 
+        border-color: transparent;
     }
 }
+
 @keyframes blink {
 
     50% {
-       border-color: transparent; 
+        border-color: transparent;
     }
 }
 </style>
