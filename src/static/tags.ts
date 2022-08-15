c7d3f36e22f0b253e8b4217c1d29006bab2f9a31
Dries1234 <driesolbrechts@gmail.com>
2022-08-15 20:43:33 +0200

    no root /

--- src/static/tags.ts
+++ src/static/tags.ts
@@ -1,46 +1,46 @@
 export const Tags = {
     rust: {
-        icon: '/icons/rust.svg',
+        icon: 'assets/icons/rust.svg',
         alt: 'rust',
     },
     typescript: {
-        icon: '/icons/typescript.svg',
+        icon: 'assets/icons/typescript.svg',
         alt: 'typescript',
     },
     vue: {
-        icon: '/icons/vue.svg',
+        icon: 'assets/icons/vue.svg',
         alt: 'vue',
     },
     vite: {
-        icon: '/icons/vite.svg',
+        icon: 'assets/icons/vite.svg',
         alt: 'vite',
     },
     node: {
-        icon: '/icons/node.svg',
+        icon: 'assets/icons/node.svg',
         alt: 'node',
     },
     docker: {
-        icon: '/icons/docker.svg',
+        icon: 'assets/icons/docker.svg',
         alt: 'docker', 
     },
     angular: {
-        icon: '/icons/angular.svg',
+        icon: 'assets/icons/angular.svg',
         alt: 'angular',
     },
     cpp: {
-        icon: '/icons/cpp.svg',
+        icon: 'assets/icons/cpp.svg',
         alt: 'cpp',
     },
     mysql: {
-        icon: '/icons/mysql.svg',    
+        icon: 'assets/icons/mysql.svg',    
         alt: 'mysql',
     },
     kotlin: {
-        icon: '/icons/kotlin.svg',
+        icon: 'assets/icons/kotlin.svg',
         alt: 'kotlin',
     },
     php: {
-        icon: '/icons/php.svg',
+        icon: 'assets/icons/php.svg',
         alt: 'php'
     },
 }
